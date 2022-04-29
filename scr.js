a5bf22ec3b743ee93b71d21b8de550229dca9485
Gustavo zero bala <98385777+gustawebp@users.noreply.github.com>
2022-04-29 19:23:53 -0300

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,9 @@
+function show() {
+    adress = document.getElementById('adress')
+    adress.innerHTML='Av. Francisco Rodrigues Filho, 3500 - Vila Suissa, Mogi das Cruzes - SP, 08810-000'
+}
+
+function tel() {
+    tel = document.getElementById('tel')
+    tel.innerHTML  = '(11) 2819-0825'
+}
